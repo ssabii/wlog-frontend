1cb002d582b8d0cacd5ec5f3a6ad13e263254a61
ssabi <fhy1930@gmail.com>
2022-10-02 22:30:09 +0900

    chore: sentry 제거

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,36 +1,11 @@
-// const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const merge = require("webpack-merge");
 
-// const { version } = require("./package.json");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "nosources-source-map",
-  plugins: [
-    new CopyPlugin([{ from: "public", to: "" }]),
-    // new SentryWebpackPlugin({
-    //   release: version,
-    //   include: "./dist",
-    //   setCommits: {
-    //     repo: "meshkorea/",
-    //     auto: true,
-    //     ignoreMissing: true,
-    //   },
-    //   ignore: [
-    //     "coverage",
-    //     "node_modules",
-    //     "scripts",
-    //     "assetsTransformer.js",
-    //     "babel.config.js",
-    //     "test.setup.js",
-    //     "test.shim.js",
-    //     "webpack.config.js",
-    //     "vendor",
-    //   ],
-    //   silent: true,
-    // }),
-  ],
+  plugins: [new CopyPlugin([{ from: "public", to: "" }])],
   stats: "minimal",
 });
