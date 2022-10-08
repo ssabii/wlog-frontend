55f6e02510219f288e9e6a885bc0c81e18673123
ssabi <fhy1930@gmail.com>
2022-10-09 02:53:12 +0900

    fix: login store 수정

--- src/modules/auth/password-login/PasswordLoginStore.ts
+++ src/modules/auth/password-login/PasswordLoginStore.ts
@@ -8,9 +8,6 @@ export default class PasswordLoginStore {
   @observable
   public form: PasswordLoginForm = new PasswordLoginForm();
 
-  @observable
-  public isLoading = false;
-
   private core: Core;
 
   constructor(core: Core) {
@@ -18,18 +15,7 @@ export default class PasswordLoginStore {
   }
 
   @action
-  // eslint-disable-next-line require-await
-  public login = () => {
-    this.isLoading = true;
-    try {
-      // await this.core.auth.login(this.form.username, this.form.password);
-    } catch (e) {
-      this.core.dialog.openAlert({
-        title: "로그인 실패",
-        message: "로그인에 실패하였습니다.",
-      });
-    } finally {
-      this.isLoading = false;
-    }
+  public login = async () => {
+    await this.core.auth.login(this.form.username, this.form.password);
   };
 }
