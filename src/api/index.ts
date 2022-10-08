eb66621834c686c8042897e0df4adf1c1e1192a8
ssabi <fhy1930@gmail.com>
2022-10-09 02:42:31 +0900

    feat: axios 에러 처리 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,25 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 import config from "config";
 
 const client = axios.create({
   baseURL: config.api.baseURL,
 });
+
+client.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response?.data) {
+      return Promise.reject(new Error(error.response.data));
+    }
+
+    if (error.message === "Network Error") {
+      return Promise.reject(new Error("네트워크 오류가 발생했습니다."));
+    }
+
+    return Promise.reject(new Error("클라이언트에서 오류가 발생했습니다."));
+  },
+);
 
 export const setHeaders = (accessToken: string, refreshToken: string) => {
   client.defaults.headers.common.Authorization = accessToken;
