a95e3cd9445c0659cc680e241c59988ad79ba147
ssabi <fhy1930@gmail.com>
2022-10-11 01:05:31 +0900

    feat: Layout path에 따라 다른 렌더링

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,10 @@
+import { GlobalNavigationBar } from "@meshkorea/vroong-design-system-web";
 import { observer } from "mobx-react";
 import React from "react";
 // @ts-ignore
 import Div100vh from "react-div-100vh";
+import { Route, Switch } from "react-router";
+import styled from "styled-components";
 
 import AlertWrapper from "components/AlertWrapper";
 import SpinnerWrapper from "components/SpinnerWrapper";
@@ -11,11 +14,27 @@ interface LayoutProps {
 }
 
 const Layout = observer(({ children }: LayoutProps) => (
-  <Div100vh>
-    {children}
+  <Body>
+    <Switch>
+      <Route path={["/login", "/register"]}>{children}</Route>
+      <Route path={["/main"]}>
+        <GlobalNavigationBar />
+        <Main>{children}</Main>
+      </Route>
+    </Switch>
     <AlertWrapper />
     <SpinnerWrapper />
-  </Div100vh>
+  </Body>
 ));
 
 export default Layout;
+
+const Body = styled(Div100vh)`
+  padding: 16px;
+  background-color: ${({ theme }) => theme.palette.core.BackgroundSecondary};
+`;
+
+const Main = styled.main`
+  height: 100%;
+  padding-top: 40px;
+`;
