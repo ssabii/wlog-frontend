78b748941fff214f18f38884411be2c550a1be99
ssabi <fhy1930@gmail.com>
2022-10-10 20:49:36 +0900

    fix: MainPage로 이름 변경

--- src/routes/main/MainPage.tsx
+++ src/routes/main/MainPage.tsx
@@ -1,43 +1,11 @@
-import { ButtonV2 } from "@meshkorea/vroong-design-system-web";
-import React, { useCallback } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-import { useCore } from "core";
+import { WorkBody } from "modules/work";
 
-const HomePage = () => {
-  const core = useCore();
+const MainPage = () => (
+  <div>
+    <WorkBody />
+  </div>
+);
 
-  const handleAlertOpen = useCallback(() => {
-    core.dialog.openAlert({
-      title: "Alert!",
-      message: "Alert은 이렇게 나옵니다.",
-    });
-  }, [core.dialog]);
-
-  const handleSpinnerOpen = useCallback(() => {
-    core.dialog.openSpinner();
-    setTimeout(() => {
-      core.dialog.closeSpinner();
-    }, 3000);
-  }, [core.dialog]);
-
-  return (
-    <div>
-      <div>
-        <h1>Home</h1>
-        <ButtonV2 onClick={handleAlertOpen}>Open alert</ButtonV2>
-        <ButtonV2 onClick={handleSpinnerOpen}>Open spinner</ButtonV2>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default HomePage;
+export default MainPage;
